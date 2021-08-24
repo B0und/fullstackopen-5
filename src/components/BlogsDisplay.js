c4124d181a871aebe8d4bff4c530baa33679c09d
b0und <f1arenova1@gmail.com>
2021-08-24 21:11:19 +0300

    feat: sort blogs by number of likes

--- src/components/BlogsDisplay.js
+++ src/components/BlogsDisplay.js
@@ -13,6 +13,10 @@ const BlogsDisplay = ({ blogs, user, setBlogs }) => {
       <h2>blogs</h2>
       {blogs
         .filter((blog) => blog.user?.username === user.username)
+        .sort((a, b) => {
+          if (a.likes > b.likes) return -1;
+          return a.likes < b.likes ? 1 : 0;
+        })
         .map((blog) => {
           return <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />;
         })}
