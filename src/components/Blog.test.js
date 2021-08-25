cb62473f9b9171a42be767e0d36e517a5c674b43
b0und <f1arenova1@gmail.com>
2021-08-25 17:01:16 +0300

    feat: test like btn press

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -66,4 +66,36 @@ test('show more adds extra info', () => {
     'like'
   )
 
+  expect(component.container).toHaveTextContent(
+    '0'
+  )
+
+})
+
+
+test('like button press works', () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    url: 'www.google.com',
+    author: 'jack',
+    likes:0,
+    user: {
+      user: 'bro',
+      username: 'anon'
+    }
+  }
+  const mockHandler = jest.fn()
+
+  const component = render(
+    <Blog blog={blog} updateLikes={mockHandler} />
+  )
+
+  const showButton = component.container.querySelector('.showMore')
+  fireEvent.click(showButton)
+
+  const likeButton = component.getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
